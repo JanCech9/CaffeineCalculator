dc386c1d17720d0f21e7a4ece734c6bc69481846
Jan Čech <jan.cech@osc.cz>
2024-04-29 08:31:24 +0200

    project files add

---
+++ CoffeeCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeCounter
+{
+  public static class CoffeeCalculator
+  {
+    public const double EsCoef = ((float)212 / 100), V60Coef = ((float)145 / 240);
+    public static double CalculateCaffeine(double es = 0, double v60 = 0)
+    {
+      double total = EsMl2Mg(es) + V60Ml2Mg(v60); 
+
+      return total;
+    }
+
+    public static double EsMl2Mg(double ml)
+    {
+      double mg = ml * EsCoef;
+
+      return mg;
+    }
+
+    public static double V60Ml2Mg(double ml)
+    {
+      double mg = ml * V60Coef;
+      
+      return mg;
+    }
+  }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoffeeCounter
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      double esMl = double.Parse(tbx_ess.Text);
+      double vMl = double.Parse(tbx_V60.Text);
+      lbl_CafCon.Content = Math.Round(CoffeeCalculator.CalculateCaffeine(esMl, vMl)) + " mg of Caffeine";
+    }
+
+    private void tbx_ess_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+      tbx_ess.Text = string.Empty;
+    }
+
+    private void tbx_V60_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+      tbx_V60.Text = string.Empty;
+
+    }
+  }
+}
