bd52a0687de63ea5364947d83ed55d7ea76e5531
Jan Čech <jan.cech@osc.cz>
2024-05-07 10:49:48 +0200

    feat: making app more robust

--- CoffeeCalculator.cs
+++ CoffeeCalculator.cs
@@ -10,12 +10,7 @@ namespace CoffeeCounter
   public static class CoffeeCalculator
   {
     public const double EsCoef = ((float)212 / 100), V60Coef = ((float)145 / 240);
-    public static double CalculateCaffeine(double es = 0, double v60 = 0)
-    {
-      double total = EsMl2Mg(es) + V60Ml2Mg(v60); 
 
-      return total;
-    }
 
     public static double EsMl2Mg(double ml)
     {
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,9 +24,20 @@ namespace CoffeeCounter
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-      double esMl = double.Parse(tbx_ess.Text);
-      double vMl = double.Parse(tbx_V60.Text);
-      lbl_CafCon.Content = Math.Round(CoffeeCalculator.CalculateCaffeine(esMl, vMl)) + " mg of Caffeine";
+      double sum = 0;
+      if (IsValid(tbx_ess.Text))
+      {
+        double esMl = double.Parse(tbx_ess.Text);
+        sum += Math.Round(CoffeeCalculator.EsMl2Mg(esMl));
+      }
+      if (IsValid(tbx_V60.Text))
+      {
+        double vMl = double.Parse(tbx_V60.Text);
+        sum += Math.Round(CoffeeCalculator.V60Ml2Mg(vMl));
+      }
+
+      lbl_CafCon.Content = sum + " mg of Caffeine";
+
     }
 
     private void tbx_ess_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -37,6 +49,25 @@ namespace CoffeeCounter
     {
       tbx_V60.Text = string.Empty;
 
+    }
+    private bool IsValid(string input)
+    {
+      if (input == "" || input.EndsWith("(ml)"))
+        return false;
+
+      return true;
+    }
+
+    private void tbx_ess_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+      Regex regex = new Regex("[^0-9]+");
+      e.Handled = regex.IsMatch(e.Text);
+    }
+
+    private void tbx_V60_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+      Regex regex = new Regex("[^0-9]+");
+      e.Handled = regex.IsMatch(e.Text);
     }
   }
 }
